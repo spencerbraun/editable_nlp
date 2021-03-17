4f25a8f573ead72780a13399f3d95cdd3e670601
Spencer Braun <spencer.braun@gmail.com>
2021-03-17 04:00:27 +0000

    eval on test

--- src/runevals.sh
+++ src/runevals.sh
@@ -17,5 +17,5 @@
 # python evaluate.py --model_path ../models/model_epoch0_ts6000.20210314.16.03.1615740151 --edit_steps 5
 
 python evaluate.py --model_path ../models/model_epoch0_ts10000.20210314.16.03.1615740151 --edit_steps 1 --test_set
-# python evaluate.py --model_path ../models/finetune/gpt2_epoch0_ts10000.20210310.18.03.1615401990 --edit_steps 1 --test_set
-# python evaluate.py --ots --edit_steps 1 --test_set
+python evaluate.py --model_path ../models/finetune/gpt2_epoch0_ts10000.20210310.18.03.1615401990 --edit_steps 1 --test_set
+python evaluate.py --ots --edit_steps 1 --test_set
