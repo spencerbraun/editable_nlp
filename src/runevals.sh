9d8bcd95eaadc3bdde37eb21a7b678834d07cfc5
Spencer Braun <spencer.braun@gmail.com>
2021-03-18 02:30:15 +0000

    eval results from Lbase models

--- src/runevals.sh
+++ src/runevals.sh
@@ -17,5 +17,9 @@
 # python evaluate.py --model_path ../models/model_epoch0_ts6000.20210314.16.03.1615740151 --edit_steps 5
 
 # python evaluate.py --model_path ../models/model_epoch0_ts10000.20210314.16.03.1615740151 --edit_steps 1 --test_set
-python evaluate.py --model_path ../models/finetune/gpt2_epoch0_ts10000.20210310.18.03.1615401990 --edit_steps 1 --test_set
-python evaluate.py --ots --edit_steps 1 --test_set
+# python evaluate.py --model_path ../models/finetune/gpt2_epoch0_ts10000.20210310.18.03.1615401990 --edit_steps 1 --test_set
+# python evaluate.py --ots --edit_steps 1 --test_set
+
+
+python evaluate.py --model_path ../models/model_epoch0_ts10000.20210317.20.03.1616014684 --edit_steps 1
+python evaluate.py --model_path ../models/model_epoch0_ts10000.20210317.22.03.1616021748 --edit_steps 1
