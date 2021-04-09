54fbbdcf5cf26c47d61df63b812c4032e7977e36
Spencer Braun <spencer.braun@gmail.com>
2021-04-08 19:14:35 -0700

    add output from learnable lr runs

--- src/config.py
+++ src/config.py
@@ -31,13 +31,13 @@ class EditConfig:
 class SelfSampleConfig:
     def __init__(self):
         self.task = 'self_sample'
-        self.inner_lr = 1e-2
+        self.inner_lr = 1e-3
         self.outer_lr = 1e-5
         self.epochs = 1
         self.max_training_samps = 1.5e4
         self.n_edit_steps = 1
         self.cedit = 1
-        self.cloc = 10
+        self.cloc = 1
         self.learnable_lr = True
 
         self.debug = False
