61eae5b664d8cbf800523bef928cc279689d3d5e
Spencer Braun <spencer.braun@gmail.com>
2021-05-01 13:56:17 -0700

    data preparation in training loop for t5

--- src/masked_lm/config.py
+++ src/masked_lm/config.py
@@ -10,3 +10,27 @@ class TrainConfig:
         self.write_loc = '..'
 
         self.silent = False
+
+
+class SelfSampleConfig:
+    def __init__(self):
+        self.task = 'T5_self_sample'
+        self.model_type = 'T5'
+        self.inner_loop = 'template' #sentence, template, or random
+
+        self.inner_lr = 1e-2
+        self.outer_lr = 1e-5
+        self.epochs = 1
+        self.max_iter = 20000
+        self.n_edit_steps = 1
+        self.cedit = 0.5
+        self.cloc = 1
+        self.learnable_lr = True
+        self.lr_lr = 1e-3
+
+        self.debug = False
+        self.model_save_pt = 5000
+        self.write_loc = '..'
+        self.ft_model_name = ""
+
+        self.silent = False
--- src/masked_lm/train.py
+++ src/masked_lm/train.py
@@ -16,6 +16,11 @@ import data
 import utils
 from config import TrainConfig
 
+import sys
+sys.path.insert(0, '../..')
+from src.train import EditableTrainer
+
+
 class T5Trainer:
     def __init__(self, config, train, validation, model_path=None):
 
@@ -153,6 +158,330 @@ class T5Trainer:
             self.train_step()
 
         self.saveState(self.model, self.train_iter)
+
+class SelfSampleTrainer(EditTrainer):
+    def __init__(self, config, dataloader, model_path=None):
+        super().__init__(config, dataloader, model_path)
+
+        self.validation_set = utils.retrieveEditDataloader(
+            self.tokenizer,
+            bs=1,
+            data_loc=self.config.write_loc,
+            dataset='validation',
+            self_sample=True
+        )
+        
+        self.model = utils.loadTrainedModel(
+            f"{self.config.write_loc}/models/finetune/{self.config.ft_model_name}", 
+            cache_dir=self.config.write_loc,
+            tokenizer=False
+        )
+        self.model.eval()
+        self.model.to(self.device)
+
+        self.model_type = self.config.model_type
+
+        if self.config.split_params:
+            ConditionedLinear.add_conditioners(self.model)
+        
+    def genModelText(self, lm_tokens):
+        
+        len_lm = lm_tokens.shape[-1]
+        edit_loc = max(random.randint(int(len_lm*0.6), int(len_lm*0.9)), 15)
+        input_ids = lm_tokens[:, :edit_loc]
+        input_size = input_ids.size()[-1]
+        
+        print("generating")
+        output_sequence = self.model.generate(
+            input_ids=input_ids,
+            max_length=input_size + 5,
+            temperature=1.2,
+            do_sample=True,
+            repetition_penalty=5.0,
+            num_return_sequences=10,
+        )
+
+        edit_tokens = random.choice(output_sequence).unsqueeze(0)
+        edit_mask = torch.ones(edit_tokens.shape, dtype=torch.long)
+        edit_labels = torch.zeros(edit_tokens.shape, dtype=torch.long) - 100
+        edit_labels[:, input_size:] = edit_tokens[:, input_size:]
+        edit_labels = edit_labels.to(self.device)
+        edit_tokens, edit_mask = edit_tokens.to(self.device), edit_mask.to(self.device)
+
+        return edit_tokens, edit_mask, edit_labels
+
+    def processMaskedLMData(self, masked_sentence, template, obj, edit_obj):
+        il = self.config.inner_loop
+        il_select = (np.random.randint(0,2) if il == 'random' else 1 if il == 'sentence' else 0)
+        lm_data = masked_sentence if il_select == 0 else template
+        edit_data = masked_sentence if il_select == 1 else template 
+
+        lm_tokens, lm_mask = lm_data
+        lm_tokens, lm_mask = lm_tokens.to(self.device), lm_mask.to(self.device)
+
+        lm_labels, _ = obj
+        lm_labels = lm_labels.to(self.device)
+
+        edit_tokens_batch, edit_mask_batch = tuple(zip(*edit_example))
+        edit_tokens, edit_mask = (
+            edit_tokens.to(self.device), 
+            edit_mask.to(self.device)
+            )
+
+        edit_labels, _ = edit_obj
+        edit_labels = edit_labels.to(self.device)
+        
+        # List of tuples
+        edit_batch = [_process_edit_tokens(et, em) for (et, em) in
+                      zip(edit_tokens_batch, edit_mask_batch)]
+
+        # Tuple of lists
+        edit_tokens, edit_mask, edit_labels, edit_locs, gold_tokens = tuple(zip(*edit_batch))
+
+        return (
+            lm_tokens, lm_mask, lm_labels, 
+            edit_tokens, edit_mask, edit_labels, 
+            edit_locs, gold_tokens
+            )
+
+    
+    def processLMData(self, lm_data, edit_example):
+        lm_tokens, lm_mask = lm_data[0]
+        lm_tokens, lm_mask = lm_tokens.to(self.device), lm_mask.to(self.device)
+        lm_labels = lm_tokens.masked_fill(lm_mask == 0, -100)
+
+        edit_tokens_batch, edit_mask_batch = tuple(zip(*edit_example))
+
+        # remove left padding
+        def _process_edit_tokens(edit_tokens, edit_mask):
+            edit_tokens = edit_tokens.squeeze(0)
+            indices = edit_tokens != self.tokenizer.pad_token_id
+            edit_tokens = edit_tokens[indices].unsqueeze(0)
+            edit_mask = edit_mask.squeeze(0)
+            edit_mask = edit_mask[indices].unsqueeze(0)
+
+            edit_labels = torch.zeros(edit_tokens.shape, dtype=torch.long) - 100
+            edit_loc = edit_tokens.shape[-1] - 5 - 1  # minus 1 for newline token
+            edit_locs = torch.tensor([edit_loc + i for i in range(5)])
+            edit_labels[:, edit_locs] = edit_tokens[:, edit_locs]
+            gold_tokens = edit_tokens[:, edit_locs]
+
+            edit_labels = edit_labels.to(self.device)
+            edit_tokens, edit_mask = edit_tokens.to(self.device), edit_mask.to(self.device)
+
+            gold_tokens = gold_tokens.cpu()
+
+            return edit_tokens, edit_mask, edit_labels, edit_locs, gold_tokens
+
+        # List of tuples
+        edit_batch = [_process_edit_tokens(et, em) for (et, em) in
+                      zip(edit_tokens_batch, edit_mask_batch)]
+
+        # Tuple of lists
+        edit_tokens, edit_mask, edit_labels, edit_locs, gold_tokens = tuple(zip(*edit_batch))
+
+        return lm_tokens, lm_mask, lm_labels, edit_tokens, edit_mask, edit_labels, edit_locs, gold_tokens
+
+    def processData(self):
+
+        return self.processLMData if self.model_type == 'gpt2' else self.processMaskedLMData
+
+    def validateSelfSampleTraining(self):
+        self.model.eval()
+
+        for ds in ['train', 'val']:
+            data = self.validation_set if ds == 'val' else self.data
+
+            ppl_pre_hist = []
+            ppl_post_hist = []
+            ll_change_hist = []
+            loss_hist = []
+
+            indices = [idx % len(data) for idx in range(self.val_iter, self.val_iter + 10)]  # select 10 elements
+            subset = Subset(data.dataset, indices)
+            for batch_idx, (lm_data, edit_example, _, _) in enumerate(subset):
+                lm_tokens, lm_mask, lm_labels, edit_tokens, edit_mask, edit_labels, edit_locs, gold_tokens = self.processLMData(lm_data, edit_example)
+
+                orig_ppl = self.perplexity(self.model, data)
+                model_out, logit_hist, ll_change, loss = performOneEdit(
+                    self.model,
+                    self.lrs,
+                    edit_tokens[0], 
+                    edit_mask[0], 
+                    edit_labels[0],
+                    edit_locs[0] - 1, 
+                    gold_tokens[0], 
+                    n_edit_steps=1
+                )
+                new_ppl = self.perplexity(model_out, data)
+
+                ppl_pre_hist.append(orig_ppl.cpu())
+                ppl_post_hist.append(new_ppl.cpu())
+                ll_change_hist.append(ll_change)
+                loss_hist.append(loss.detach().cpu())
+
+            metrics = {
+                f'ppl_pre_{ds}': np.mean(ppl_pre_hist),
+                f'ppl_post_{ds}': np.mean(ppl_post_hist),
+                f'll_change_{ds}': np.mean(ll_change_hist),
+                f'loss/eval_{ds}': np.mean(loss_hist),
+            }
+            self.wandb_log(self.val_iter * 100, metrics)
+
+        self.val_iter += 1
+
+        self.model.train()
+    
+    def run(self):
+        
+        if not self.config.debug:
+            torch.save(self.config, self.hyperspath)
+
+        self.model.train()
+        self.model.to(self.device)
+        opt = torch.optim.Adam(
+            self.model.parameters(), 
+            self.config.outer_lr
+            )
+        
+        global_iter = 0
+        print("Starting Training")
+        
+        self.lrs = [
+            torch.nn.Parameter(torch.tensor(self.config.inner_lr)) 
+            for p in self.model.transformer.h[-3:].parameters()
+            ]
+        lr_opt = torch.optim.Adam(self.lrs, lr=self.config.lr_lr)
+
+        skip_count = 0
+
+        for epoch in range(self.config.epochs):
+            self.epoch = epoch
+            
+            for train_step, (lm_data, edit_example, _, _) in enumerate(self.data):
+                lm_tokens, lm_mask, lm_labels, edit_tokens, edit_mask, edit_labels, edit_locs, gold_tokens = self.processLMData(lm_data, edit_example)
+
+                # Compute base loss
+                base_out = self.model(
+                    lm_tokens, 
+                    attention_mask=lm_mask,
+                    labels=lm_labels
+                )
+                l_base = base_out.loss
+                l_base.backward()
+
+                # Cache the current params and grads since we're going to modify the model during
+                #  the edit process
+                p_cache = {}
+                for n, p in self.model.named_parameters():
+                    p_cache[n] = (p.data.detach().clone(), p.grad.data.detach().clone())
+
+                for edit_example_idx in range(self.config.n_edits):
+                    param_groups = [
+                        {'params': p, 'lr': None} 
+                        for p in self.model.transformer.h[-3:].parameters()
+                    ]
+                    inner_opt = (
+                        torch.optim.SGD(param_groups) if self.config.learnable_lr
+                        else torch.optim.SGD(
+                                self.model.transformer.h[-3:].parameters(), 
+                                lr=self.config.inner_lr
+                        )
+                    )
+                    with higher.innerloop_ctx(
+                            self.model, 
+                            inner_opt, 
+                            override={'lr': self.lrs} if self.config.learnable_lr else None,
+                            copy_initial_weights=False, 
+                            track_higher_grads=True
+                    ) as (fmodel, diffopt):
+                        if self.config.split_params:
+                            fmodel.set_editing(True)
+
+                        for edit_step in range(self.config.n_edit_steps):
+                            loss = fmodel(
+                                edit_tokens, 
+                                attention_mask=edit_mask,
+                                labels=edit_labels
+                            ).loss
+                            diffopt.step(loss)
+
+                        if self.config.split_params:
+                            fmodel.set_editing(False)
+
+                        edit_out = fmodel(
+                            edit_tokens, 
+                            attention_mask=edit_mask,
+                            labels=edit_labels
+                        )
+                        l_edit = edit_out.loss
+
+                        edited_base_out = fmodel(
+                            lm_tokens, 
+                            attention_mask=lm_mask,
+                            labels=lm_labels
+                        )
+
+                        l_loc = (
+                            F.softmax(base_out.logits.detach(), dim=-1) *
+                            (
+                                F.log_softmax(base_out.logits.detach(), dim=-1) - 
+                                F.log_softmax(edited_base_out.logits, dim=-1)
+                            )).sum(-1).mean()
+
+                        total_edit_loss = (
+                            self.config.cloc * l_loc  + 
+                            self.config.cedit * l_edit
+                        ) / self.config.n_edits
+                        total_edit_loss.backward()
+
+                        for fp, p in zip(fmodel.parameters(), self.model.parameters()):
+                            p.data[:] = fp.data.detach().clone()
+
+                    # It only makes sense to train more than one edit if we've split the params
+                    if not self.config.split_params:
+                        break
+
+                # merge gradients from edit training and restore pre-edit parameters
+                for n, p in self.model.named_parameters():
+                    if p.grad is not None:
+                        p.grad += p_cache[n][1]
+                    else:
+                        p.grad = p_cache[n][1]
+
+                    p.data = p_cache[n][0]
+
+                # accumulate grads 
+                if train_step % 5 == 0:
+                    opt.step()
+                    opt.zero_grad()
+                
+                    if self.config.learnable_lr:
+                        lr_opt.step()
+                        lr_opt.zero_grad()
+                
+                global_iter += 1
+                
+                loss_dict = {
+                    "loss/base": l_base, "loss/edit": l_edit, 
+                    "loss/loc": l_loc, "loss/train": total_loss
+                    }
+                self.echo(train_step, **loss_dict)
+                loss_dict.update({f"lr/lr{i}":lr.data.item() for i, lr in enumerate(self.lrs)})
+                self.wandb_log(global_iter, loss_dict)
+                self.saveState(self.model, global_iter, name="self_sample")
+                if self.config.learnable_lr:
+                    self.saveState(self.lrs, global_iter, name='lr')
+                if global_iter >= self.config.max_iter:
+                    print("Reached max iterations")
+                    break
+            
+                if (train_step % 100 == 0) & (not self.config.debug):
+                    self.validateSelfSampleTraining()
+        
+        self.saveState(self.model, global_iter, final=True, name="self_sample")
+        if self.config.learnable_lr:
+            self.saveState(self.lrs, global_iter, final=True, name='lr')
 
 
 if __name__ == "__main__":
